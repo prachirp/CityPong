76ef15f82639ebb7851be2272bc690005beadb40
prachirp <prachi.pendse@rutgers.edu>
2015-04-19 12:04:28 -0400

    Pong with a City Background

---
+++ Assets/PlayerControls.js
@@ -0,0 +1,24 @@
+﻿#pragma strict
+
+var moveUp : KeyCode;
+var moveDown : KeyCode;
+var speed : float = 10;
+
+function Start () {
+}
+
+function Update () {
+	
+	var rb = GetComponent(Rigidbody2D);
+
+	if(Input.GetKey(moveUp)){	
+		rb.velocity.x = speed;
+	}
+	else if(Input.GetKey(moveDown)){
+		rb.velocity.x = speed * -1;
+	}
+	else{
+		rb.velocity.x = 0;
+	}
+	
+}
